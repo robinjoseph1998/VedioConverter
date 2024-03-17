69b67aa3da83691922dc8a91fd88ca29a0bdff96
Robin Joseph <robinjoseph11998@gmail.com>
2024-03-18 01:30:36 +0530

    route set\

--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,11 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"VedioConverter/controllers"
+
+	"github.com/gin-gonic/gin"
+)
 
 func SetupRouters(r *gin.Engine) {
-
+	r.POST("/mp4tomkv", controllers.Mp4ToMkvConverter)
 }
